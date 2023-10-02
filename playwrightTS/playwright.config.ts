fbc7b96be1c3bfa2d3d33061245a12dffbca044b
NaveenChittimodu <naveenchittimodu@atmecs.com>
2023-10-02 15:09:05 +0530

    playwrighttypes

---
+++ playwrightTS/playwright.config.ts
@@ -0,0 +1,26 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+
+  // testMatch : ["Loginpage.spec.ts"],
+  // testMatch : ["alerts.test.ts"],
+  testMatch : ["filedownload.ts"],
+  
+  use:{
+    baseURL: 'https://bookcart.azurewebsites.net/login',
+    headless: false,
+    screenshot:"on",
+    video:"off"
+  },
+
+  reporter:[["dot"],["json",{
+
+    outputFile:"jsonFileReport/jsonReport.json"
+
+  }], ["html",{
+
+      open: "never"
+  }]]
+
+};
+export default config;
